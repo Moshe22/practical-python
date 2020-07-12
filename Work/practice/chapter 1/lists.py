de44d14c79937976c19b2a9e38664982a51ef4e5
moshe22 <moshe22@live.unc.edu>
2020-07-12 12:05:14 -0400

    lists

---
+++ Work/practice/chapter 1/lists.py
@@ -0,0 +1,21 @@
+symbols = 'HPQ,AAPL,IBM,MSFT,YHOO,DOA,GOOG'
+symlist = symbols.split(',')
+print(symlist)
+print('symlist[0]',symlist[0])
+print('symlist[-1]', symlist[-1])
+print('symlist[-2]', symlist[-2])
+symlist[2] = 'AIG'
+print(symlist)
+print(symlist[0:3])
+print(symlist[-2:])
+mysyms = []
+mysyms.append('GOOG')
+print('mysyms', mysyms)
+
+# this method resizes list
+symlist[-2:] = mysyms
+print('symlist', symlist)
+
+#1.20
+for s in symlist:
+    print('s=',s)
