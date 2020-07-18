6d8ef6f431a460e061dfe7faf11489b6d7dfa458
moshe22 <moshe22@live.unc.edu>
2020-07-18 19:12:14 -0400

    finished lists

--- Work/practice/chapter 1/lists.py
+++ Work/practice/chapter 1/lists.py
@@ -19,3 +19,36 @@ print('symlist', symlist)
 #10
 for s in symlist:
     print('s=',s)
+print('AIG' in symlist)
+
+#1.22
+symlist.append('RHT')
+print('1.22', symlist)
+
+#1.22 insert
+symlist.insert(1, 'AA')
+print('1.22 insert', symlist)
+
+#1.22 remove
+symlist.remove('MSFT')
+print('1.22 remove', symlist)
+
+#1.22 append at end
+symlist.append('YHOO')
+print('1.22 append at end', symlist)
+print('index of first YHOO', symlist.index('YHOO'))
+print('number of YHOO', symlist.count('YHOO'))
+symlist.remove('YHOO')
+symlist.sort()
+print('sorted',symlist)
+
+symlist.sort(reverse=True)
+print('reverse sorted', symlist)
+
+#1.24 joining
+a=','.join(symlist)
+b=':'.join(symlist)
+c=''.join(symlist)
+print('comma join',a)
+print('colon join',b)
+print('no space join',c)
